adedc6bb19985531b3ee2d3f0fd72212e8a5e8f4
dladncks1217 <dlaxodud1217@gmail.com>
2022-12-26 17:17:21 +0900

    test(DomainTest-isAlreadyEatem): 이미 먹은 음식 검증 메서드 테스트

--- __tests__/DomainTest.js
+++ __tests__/DomainTest.js
@@ -15,4 +15,14 @@ describe('도메인 단위 테스트', () => {
     Random.shuffle.mockReturnValueOnce([4, 1, 2, 3, 5, 6, 7, 8, 9]);
     expect(coach.suggestMenu(1, verify.eatenTwice, getSuggestMenu)).toBe('된장찌개');
   });
+
+  test('2번 이상 먹은 메뉴인지 확인', () => {
+    const coach = new Coach('haha');
+    Random.shuffle = jest.fn();
+    Random.shuffle.mockReturnValueOnce([4, 1, 2, 3, 5, 6, 7, 8, 9]);
+    Random.shuffle.mockReturnValueOnce([4, 2, 5, 1, 4, 8, 6, 9, 7]);
+    coach.suggestMenu(1, verify.eatenTwice, getSuggestMenu);
+    coach.suggestMenu(1, verify.eatenTwice, getSuggestMenu);
+    expect(coach.isAlreadyEatenTwice('된장찌개')).toBe(true);
+  });
 });
